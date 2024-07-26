d2a2fecc871a9579accba8430f7230b9d209b47f
Abdul ahad <abdulahad30396@gmail.com>
2024-07-26 11:04:41 +0500

    some work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <h4> Main Page </h4>
+    <div style={{ textAlign:"center" }} >
+      <h1> ebike.pk </h1>
+      <h6> We have taken the side down for some brief maintenance. </h6>
+      <h6> Stay Connected! </h6>
+      <h6> Sorry for inconvenience   </h6>
+    </div> 
   );
 }
