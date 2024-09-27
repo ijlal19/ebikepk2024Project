19ffb430ae7ca02a99381cb27a03731c6ec38344
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-09-27 17:43:38 +0500

    ,m,

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import AboutUsComp from "@/pageLayouts/about-us/index"
 export default function AboutUs() {
     return (
-        <AboutUsComp/>
+        <AboutUsComp/> 
     )
 }
