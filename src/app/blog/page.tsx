68656ee09875204a3151710b236d1c92e370867b
Abdul ahad <abdulahad30396@gmail.com>
2024-09-27 17:42:47 +0500

    update

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import BlogComp from "@/pageLayouts/blog/index"
+export default function Blog() { 
+    return (
+        <BlogComp/>
+    )
+}
---
+++ src/pageLayouts/blog/index.jsx
@@ -0,0 +1,11 @@
+import { Box, Container } from '@mui/material'
+import styles from './index.module.scss'
+const AboutUs = () => {
+    return (
+        <Box className={styles.blog_main}>
+            
+        </Box>
+    )
+}
+
+export default AboutUs
