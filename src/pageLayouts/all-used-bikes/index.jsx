a1fb59dbceaaf01b8372421e1a2353b3bcc3cd53
Abdul ahad <abdulahad30396@gmail.com>
2024-10-01 18:12:47 +0500

    push

--- src/pageLayouts/all-used-bikes/index.jsx
+++ src/pageLayouts/all-used-bikes/index.jsx
@@ -1,8 +1,9 @@
 'use client'
-import { Box, Container } from '@mui/material'
+import { Box, Container, Grid } from '@mui/material'
 import styles from './index.module.scss'
 import React, { useState, useEffect } from 'react'
 import { getAllbikesDetail } from "@/functions/globalFuntions"
+import {Apps, FormatListBulleted } from '@mui/icons-material';
 
 const AllUsedBike = () => {
    
@@ -27,18 +28,11 @@ const AllUsedBike = () => {
                         {/* sort Bar */}
                         <div className={styles.navigation}>
                             <div className={styles.text_container}>
-                                <span className={styles.sort_text}>Sort By:</span>
-                                <select>
-                                    <option>Updated Date: Recent First</option>
-                                </select>
+                                <span className={styles.bike_text}> Used Bikes </span>
                             </div>
-                            <div className={styles.button_container}>
-                                <button>
-                                    <span>LIST</span>
-                                </button>
-                                <button>
-                                    <span>GRID</span>
-                                </button>
+                            <div className={styles.swap_button_container}>
+                                <span> <Apps className={styles.swap_icon}/> </span>
+                                <span> <FormatListBulleted className={styles.swap_icon}/> </span>
                             </div>
                         </div>
 
@@ -50,14 +44,14 @@ const AllUsedBike = () => {
                         {
                             allBikesArr.length > 0 && allBikesArr.map((val, ind) => {
                                 return(
-                                    <div className={styles.card} key={ind}>
+                                    <div className={styles.long_card} key={ind}>
                                         <div className={styles.bike_image}>
                                             {val.images && val.images.length > 0 ? <img src={val.images[0]} alt={'a'} className={styles.card_image} /> : "" }
                                         </div>
                                     
                                         <div className={styles.card_info}>
                                         <h2 className={styles.card_title}> {val.title} </h2>
-                                        <h3 className={styles.product_price}>PKR {val.price}</h3>
+                                        <h3 className={styles.card_price_desktop}>PKR {val.price}</h3>
                                         <p className={styles.card_details}> {val?.city?.city_name} </p>
                                         <ul className={styles.bike_details}>
                                             <li>{val?.year?.year}</li>
@@ -67,9 +61,10 @@ const AllUsedBike = () => {
                                             <li>4 Stroke</li>
                                         </ul>
                                         
-                                        <p className={styles.card_price}>Price:  {val.price}</p>
-                                            <button className={styles.show_phone_button}> Show Phone Number </button>
-                                            {/* <p className={styles.phone_number}>{'aa'}</p> */}
+                                        <p className={styles.card_price_mobile}>Price:  {val.price}</p>
+                                        
+                                        <button className={styles.show_phone_button}> Show Phone Number </button>
+                                        {/* <p className={styles.phone_number}>{'aa'}</p> */}
                                         </div>
                                     </div>
                                 )
