32e83f2fd476161692197bce74a3d4d4880abdaa
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-10-18 10:08:53 +0500

    update

--- src/pageLayouts/all-new-bikes/index.tsx
+++ src/pageLayouts/all-new-bikes/index.tsx
@@ -1,10 +1,72 @@
 "use client"
-import React, { useState, useEffect } from 'react'
-import { bikesDataArr, dealerDataArr } from './data'
+import React, { useState } from 'react';
+import { bikesDataArr, dealerDataArr } from './data';
+import { Box, Button, Grid, Typography, useMediaQuery } from '@mui/material';
+import styles from './index.module.scss';
+import ImgCard from '@/sharedComponents/itemCard';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export default function AllNewBikes() {
- 
+  const [Showmore, setShowmore] = useState(true);
+  const isMobile = useMediaQuery('(max-width: 768px)');
+  const isMobile2 = useMediaQuery('(max-width: 550px)');
+
+  const showmore = () => {
+    setShowmore(!Showmore);
+  };
+
   return (
-    <> all  new bike </>
+    <Box className={styles.all_new_bike_main}>
+      <Box className={styles.description_box}>
+        <Box className={styles.card_main}>
+          <img
+            src='https://res.cloudinary.com/dtroqldun/image/upload/c_scale,f_auto,h_50,q_auto,w_auto,dpr_auto/v1540624359/bike_brands/Suzuki.png'
+            alt='./'
+            className={styles.card_image}
+          />
+        </Box>
+        <Typography className={styles.descriptionPara}>
+          {Showmore ? bikesDataArr[0].bike_brand.description.slice(0, 100) : bikesDataArr[0].bike_brand.description}
+        </Typography>
+        <Box>
+          <Box className={styles.buttons_box}>
+            <Button className={styles.show_more_button} onClick={showmore} disableRipple>
+              {Showmore ? <>Show More<KeyboardArrowDownIcon sx={{ fontSize: '15px' }} /></> : <>Show Less<KeyboardArrowUpIcon sx={{ fontSize: '15px' }} /></>}
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+      <Grid container className={styles.grid_sectiion_box}>
+        <Grid item xs={isMobile ? 12 : 9} className={styles.card_grid}>
+          <Typography className={styles.heading}>New Bikes</Typography>
+          {
+            bikesDataArr.map((e, i) => (
+              <ImgCard data={e} from='u' key={i} />
+            ))
+          }
+        </Grid>
+        <Grid item xs={isMobile ? 12 : 3} className={styles.Dealers_grid_box}>
+          <Typography className={styles.heading}>Related Dealers</Typography>
+          <Box className={styles.Dealers_card}>
+            {
+              dealerDataArr.map((e: any, i: any) => {
+                return (
+                  <Box className={styles.card_main} key={i}>
+                    <img src={e.bike_brand.logoUrl} alt='' className={styles.card_image} />
+                    <Box className={styles.card_text}>
+                      <Typography className={styles.card_title}>{e.shop_name}</Typography>
+                      <Typography className={styles.card_location}>{e.city.city_name}</Typography>
+                    </Box>
+                  </Box>
+                )
+              })
+            }
+          </Box>
+          <Button className={styles.view_detail_btn} > View Bike Detail <KeyboardArrowRightIcon/></Button>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
---
+++ src/pageLayouts/bike-brands/Card/index.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import styles from './index.module.scss'
+import { Box, Typography } from '@mui/material'
+const BikesBrandCard = ({ data }: any) => {
+  return (
+      <Box className={styles.card_main}>
+        <img src={data.logoUrl} alt={data.brandName} className={styles.card_image} />
+      </Box>
+  );
+};
+
+
+export default BikesBrandCard
