90a2b356e28ed94a6a71d2dd7034901c20d28268
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-11-02 02:28:52 +0500

    update

--- src/pageLayouts/bike-brands/Card/index.tsx
+++ src/pageLayouts/bike-brands/Card/index.tsx
@@ -1,11 +1,15 @@
 'use client'
 import React from 'react'
 import styles from './index.module.scss'
-import { Box, Typography } from '@mui/material'
+import { Box, Link, Typography } from '@mui/material'
+// import Link from 'next/link';
 const BikesBrandCard = ({ data }: any) => {
   return (
       <Box className={styles.card_main}>
-        <img src={data.logoUrl} alt={data.brandName} className={styles.card_image} />
+        <Box className={styles.card_image}>
+        <img src={data.logoUrl} alt={data.brandName} className={styles.image} />
+        </Box>
+       <Link href='' className={styles.link_text}><Typography >{data.brandName}</Typography></Link>
       </Box>
   );
 };
--- src/pageLayouts/bike-brands/index.tsx
+++ src/pageLayouts/bike-brands/index.tsx
@@ -1,32 +1,35 @@
 "use client"
 import React, { useState, useEffect } from 'react'
-import { Box,useMediaQuery } from '@mui/material';
+import { Box, Typography, useMediaQuery } from '@mui/material';
 import styles from './index.module.scss'
 import BikesBrandCard from './Card/index'
 import { getbrandData } from '@/functions/globalFuntions';
 export default function NewBikeBrand() {
 
- const [allBrandArr, setAllBrandArr] = useState([])
+  const [allBrandArr, setAllBrandArr] = useState([])
 
-    useEffect(() => {
-        fetchBrandInfo()
-    }, [])
-    async function  fetchBrandInfo() {
-        let res = await getbrandData()
-        setAllBrandArr(res)
-      }
+  useEffect(() => {
+    fetchBrandInfo()
+  }, [])
+  async function fetchBrandInfo() {
+    let res = await getbrandData()
+    setAllBrandArr(res)
+  }
 
   return (
     <Box className={styles.bikes_brand_main}>
-       {
-                allBrandArr.map((e:any,i:any)=>{
-                  return(
-                    <Box className={styles.brand_image_box} key={i}>
-                      <BikesBrandCard key={i} data={e}/>
-                    </Box>
-                  )
-                })
-              }
+      <Box className={styles.bikes_brand_container}>
+        <Typography className={styles.heading}>New Bikes By Make</Typography>
+        {
+          allBrandArr.map((e: any, i: any) => {
+            return (
+              <Box className={styles.brand_image_box} key={i}>
+                <BikesBrandCard key={i} data={e} />
+              </Box>
+            )
+          })
+        }
+      </Box>
     </Box>
   );
 }
