97a0e8400b23f67ae8f5c28da23e4fd8f77ccecd
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-11-03 02:10:37 +0500

    update

--- src/pageLayouts/about-us/index.jsx
+++ src/pageLayouts/about-us/index.jsx
@@ -4,7 +4,6 @@ const AboutUs = () => {
     return (
         <>
             <Box className={styles.about_main}>
-                <Container className={styles.about_container}>
                     <Box className={styles.aboutus}>
                         <h1 className={styles.abutus_heading}>About Us</h1>
                         <Box className={styles.about_text}>
@@ -15,7 +14,6 @@ const AboutUs = () => {
                             <p className={styles.paragraph}>Ebike.pk tag line &quot;Yahan Sirf Bike Hai&quot; also suggests that it&lsquo;s an exclusive motorcycle portal. As they not only claim Yahan Sirf Bike Hai but they actually mean it. Any individual who is looking for a new motorbike or sell a used motorbike, it&lsquo;s possible on ebike. Similarly, spare parts, accessories even any motorbike related part can get from <span style={{ color: 'blue',cursor:'pointer'}}>ebike.pk</span>.</p>
                         </Box>
                     </Box>
-                </Container>
             </Box>
         </>
     )
