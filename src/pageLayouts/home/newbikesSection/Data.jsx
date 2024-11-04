6fe21c1a545282d39eb9c229baf72e701418af15
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-11-05 02:11:18 +0500

    upda

--- src/pageLayouts/home/newbikesSection/Data.jsx
+++ src/pageLayouts/home/newbikesSection/Data.jsx
@@ -3,38 +3,38 @@ const Data = [
     url: "",
     img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1728045800/Yamaha_YB125Z_DX-20_xuqja3.jpg",
     title: "Yamaha YB125Z DX",
-    price:"PKR 100000",
+    price:"PKR 454,000",
    },
 {
     url: "",
     img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1728046737/Yamaha_YB125G-20_q5hk2x.jpg",
     title: "YB125Z",
-    price:"PKR 100000",
+    price:"PKR 485,000",
    },
    {
       url: "",
       img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1727969155/Honda_CG125-20_tertpl.jpg",
       title: "Honda 125",
-      price:"PKR 100000",
+      price:"PKR 234,900",
      },
 {
     url: "",
     img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1728047731/Suzuki_GSX_125-20_uhnujy.jpg",
     title: "YBR125G",
-    price:"PKR 100000",
+    price:"PKR 499,000",
    },
 
    {
     url: "",
     img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1728047566/Yamaha_YB_125_Z-20_u0ek4l.jpg",
     title: "Suzuki GSX125",
-    price:"PKR 100000",
+    price:"PKR 424,000",
    },
   {
       url:"",
       img_url:"https://res.cloudinary.com/dzfd4phly/image/upload/v1728050127/Suzuki_GS150-20_ig7dzi.jpg",
       title:"Suzuki GS150",
-      price:"PKR 100000",
+      price:"PKR 389,000",
    }
 ]
 export default Data
--- src/pageLayouts/home/newbikesSection/TrendingData.jsx
+++ src/pageLayouts/home/newbikesSection/TrendingData.jsx
@@ -3,44 +3,44 @@ const TrendingData = [
       url: "",
       img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1727969155/Honda_CG125-20_tertpl.jpg",
       title: "Honda 125",
-      price:"PKR 100000",
+      price:"PKR 234,900",
      },
   {
       url: "",
       img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1728045800/Yamaha_YB125Z_DX-20_xuqja3.jpg",
       title: "Yamaha YB125Z DX",
-      price:"PKR 100000",
+      price:"PKR 454,000",
      },
   {
       url: "",
       img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1728046737/Yamaha_YB125G-20_q5hk2x.jpg",
       title: "YB125Z",
-      price:"PKR 100000",
+      price:"PKR 485,000",
      },
   {
       url: "",
       img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1728047731/Suzuki_GSX_125-20_uhnujy.jpg",
       title: "YBR125G",
-      price:"PKR 100000",
+      price:"PKR 499,000",
      },
   
   {
       url: "",
       img_url: "https://res.cloudinary.com/dzfd4phly/image/upload/v1728047566/Yamaha_YB_125_Z-20_u0ek4l.jpg",
       title: "Suzuki GSX125",
-      price:"PKR 100000",
+      price:"PKR 424,000",
      },
      {
         url:"",
         img_url:"https://res.cloudinary.com/dzfd4phly/image/upload/v1728052441/Honda_CB150F_-20_sjd5c0.jpg",
         title:"Honda CB150F",
-        price:"PKR 100000",
+        price:"PKR 497,900",
      },
      {
         url:"",
         img_url:"https://res.cloudinary.com/dzfd4phly/image/upload/v1728052883/Honda_CB125F_-20_1_zfxbb4.jpg",
         title:"Honda CB125F",
-        price:"PKR 100000",
+        price:"PKR 390,900",
      }
   ]
   export default TrendingData
