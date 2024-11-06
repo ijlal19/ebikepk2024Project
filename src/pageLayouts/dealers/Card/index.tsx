7803980b5a3413fa5678a5f08cffc78c980d860d
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-11-07 03:43:40 +0500

    update

--- src/pageLayouts/dealers/Card/index.tsx
+++ src/pageLayouts/dealers/Card/index.tsx
@@ -1,7 +1,7 @@
 import styles from './index.module.scss'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export const FeatureDealerCard =({props}:any)=>{
-    console.log({props})
     return(
         <div className={styles.feature_card_main}>
             <p className={styles.shop_name}>{props.shop_name}</p>
@@ -16,17 +16,20 @@ export const FeatureDealerCard =({props}:any)=>{
 
 
 
-
-
-
-
-
-
-
-// export const DealerinPakCard =()=>{
-//     return(
-//         <>
-//         <p>Dealer In Pakistan Card</p>
-//         </>
-//     )
-// }
+export const DealerinPakCard =({props}:any)=>{
+    console.log(props)
+    return(
+        <div className={styles.dealer_card_main}>
+            <div className={styles.image_box}>
+                <img src={props.bike_brand.logoUrl} alt=""  className={styles.image}/>
+            </div>
+            <div className={styles.detail_box}>
+                <p className={styles.shop_name}>{props.shop_name}</p>
+                <p className={styles.city}>Dealer in {props.city.city_name}</p>
+                <p className={styles.address}>{props.address.slice(0,25)} ... <span style={{marginRight:7,marginLeft:7}}>|</span> Listed in 
+                <span style={{marginRight:7,marginLeft:7}}><ArrowForwardIcon sx={{fontSize:15,margin:0,color:"green"}}/></span> {props.createdAt.slice(0,10)}</p>
+                <button className={styles.more_button}>View More Details</button>
+            </div>
+        </div>
+    )
+}
--- src/pageLayouts/dealers/dealer-in-pakistan/index.tsx
+++ src/pageLayouts/dealers/dealer-in-pakistan/index.tsx
@@ -3,31 +3,62 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { DealerinPakFilter } from './filter';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AllDealerData } from '../Data';
+import { DealerinPakCard } from '../Card';
+import { Pagination } from '@mui/material';
+
 export const DealerInPakistan = () => {
     const [open, setOpen] = React.useState(false);
     const toggleDrawer = (newOpen: boolean) => () => {
         setOpen(newOpen);
     }
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
+        setCurrentPage(page);
+      };
+    const DealerPerPage = 10;
+    const totalPages = Math.ceil(AllDealerData.length / DealerPerPage);
+    const currentData = AllDealerData.slice((currentPage - 1) * DealerPerPage, currentPage * DealerPerPage);
     const DrawerList = (
-        <Box className={styles.filter_drawer_main} role="presentation" onClick={toggleDrawer(false)}>
-       <DealerinPakFilter/>
+        <Box className={styles.filter_drawer_main} role="presentation" >
+            <DealerinPakFilter />
         </Box>
     );
     return (
         <div>
-        <Button onClick={toggleDrawer(true)}>Fliter Drawer</Button>
-        <Drawer open={open}>
-            {DrawerList}
-        </Drawer>
-    </div>
+            <div className={styles.Dealer_pakistan_main}>
+                <div className={styles.heading_box}>
+                    <p className={styles.showrooms_heading}>Bike Showrooms / Dealers in Pkaistan</p>
+                    <div className={styles.search_box}>
+                        <Button onClick={toggleDrawer(true)} className={styles.drawer_button}><MenuIcon/></Button>
+                        <Drawer open={open} onClose={toggleDrawer(false)}>
+                            {DrawerList}
+                        </Drawer>
+                        <input type="text" placeholder='Search' className={styles.search_input}/>
+                    </div>
+                </div>
+                    <div className={styles.dealer_card_section}>
+                        {
+                            currentData.map((e:any,i:any)=>{
+                                return(
+                                    <DealerinPakCard props={e} key={i}/>
+                                )
+                            })
+                        }
+                    </div>
+                    <Box className={styles.pagination}>
+               <Pagination
+                 count={totalPages}
+                 page={currentPage}
+                 onChange={handlePageChange}
+                 variant="outlined"
+                 shape="rounded"
+                 color='primary'
+               />
+             </Box>
+            </div>
+        </div>
     )
 }
