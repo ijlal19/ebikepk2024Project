64821ac298dac8564963449f0b0df0c212209793
Abdul ahad <abdulahad30396@gmail.com>
2025-03-27 21:52:29 +0500

    work

--- src/app/new-bikes/[slug]/[slug2]/[slug3]/page.tsx
+++ src/app/new-bikes/[slug]/[slug2]/[slug3]/page.tsx
@@ -1,5 +1,27 @@
 import * as React from 'react';
 import NewBikeDetailsComp from "@/ebikeWeb/pageLayouts/new-bike-detail/index"
+import { Metadata } from 'next'
+import {getnewBikedetailsData } from '@/ebikeWeb/functions/globalFuntions';
+
+type Props = {
+  params: { slug3: string }
+}
+
+export async function generateMetadata({ params } : Props): Promise<Metadata> {
+  const { slug3 } = params
+  const responsedetails:any = await getnewBikedetailsData(slug3)
+
+  return {
+    title:  responsedetails?.length > 0 ? responsedetails[0].bike?.meta_title + ' | ebike.pk' : "",
+    description: responsedetails?.length > 0 ? responsedetails[0]?.bike?.meta_description + ' | ebike.pk' : "",
+    openGraph: {
+        title:  responsedetails?.length > 0 ? responsedetails.bike?.meta_title + ' | ebike.pk' : "",
+        description: responsedetails?.length > 0 ? responsedetails?.bike?.meta_description + ' | ebike.pk' : "",
+    }
+  }
+}
+
+
 export default function NewBikeDetail() {
     return (
         <NewBikeDetailsComp /> 
