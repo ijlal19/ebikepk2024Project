f4b3f51fdc7a51bbef3bdcea376208627e854e21
Abdullah30nov <159879528+Abdullah30nov@users.noreply.github.com>
2025-04-17 04:22:47 +0500

    update

--- src/ebikeShop/ShopPages/Cart/index.tsx
+++ src/ebikeShop/ShopPages/Cart/index.tsx
@@ -4,14 +4,27 @@ import { DeleteuserCart, GetUserCart } from "@/ebikeShop/Shopfunctions/globalFun
 import { Box, Grid, useMediaQuery, Typography, Button } from "@mui/material";
 import Loader from "@/ebikeShop/ShopSharedComponent/loader/loader";
 import DeleteIcon from '@mui/icons-material/Delete';
+import CloseIcon from '@mui/icons-material/Close';
 import { useEffect, useState } from "react";
 import styles from './index.module.scss';
+import Modal from '@mui/material/Modal';
+
+
 
 const MyCart = () => {
     const [IsLogin, setIsLogin] = useState<any>('not_login');
+    const[PaymentMethod,setPaymentMethod]=useState<any>('');
     const IsMobile = useMediaQuery('(max-width:768px)');
     const [isLoading, setIsLoading] = useState(false);
     const [MyCartdata, setMyCart] = useState<any>([]);
+    const[OrderNote,setOrderNote]=useState<any>('');
+    const[UserAddress,setAddress]=useState<any>('');
+    const[UserEmail,setEmail]=useState<any>('');
+    const[UserPhone,setPhone]=useState<number>();
+    const[UserName,setName]=useState<any>('');
+    const handleClose = () => setOpen(false);
+    const handleOpen = () => setOpen(true);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         let _isLoginUser = isLoginUser();
@@ -36,7 +49,7 @@ const MyCart = () => {
             return
         }
         else {
-            alert('Window reload again')
+            // alert('Window reload again')
         }
     };
 
@@ -78,58 +91,119 @@ const MyCart = () => {
         }
     }
 
+    console.log("data" , UserName)
+    const handlesubmit =(e:any)=>{
+        e?.preventDefault()
+        const obj ={
+           Userame:UserName,
+           UserEail:UserEmail,
+           UsePhone:UserPhone,
+           OrerNote:OrderNote,
+           UsrAddress:UserAddress,
+           PmentMethod:PaymentMethod
+        }
+        console.log("data" , obj)
+    }
+
     return (
         <Box className={styles.main}>
             {
                 !isLoading ?
 
-                    <Grid container className={styles.cart_container}>
-                        <Grid item xs={IsMobile ? 12 : 8} className={styles.cart_grid}>
-                            <table className={styles.table}>
-                                <thead className={styles.thead}>
-                                    <tr className={styles.trow}>
-                                        <th className={styles.th_image}>{IsMobile ? "" : "Image"}</th>
-                                        <th className={styles.th_name}>{IsMobile ? "Name" : "Product Name"}</th>
-                                        <th className={styles.th_quantity}>Quantity</th>
-                                        <th className={styles.th_price}>Price</th>
-                                        <th className={styles.th_remove}>{IsMobile ? "" : "Remove"}</th>
-                                    </tr>
-                                </thead>
-                                <tbody className={styles.tbody}>
-                                    {groupedCart.map((e: any, i: number) => (
-                                        <tr className={styles.trow} key={i}>
-                                            <td className={styles.td_image_box}>
-                                                <img src={optimizeImage(e?.product?.images[0], 'h_150', 'w_150')} alt="" className={styles.image} />
-                                            </td>
-                                            <td className={styles.td_name}>
-                                                {IsMobile ? add3Dots(e?.product?.product_name, 15) : add3Dots(e?.product?.product_name, 50)}
-                                            </td>
-                                            <td className={styles.td_quantity}>{e?.quantity}</td>
-                                            <td className={styles.td_price}>{priceWithCommas(e?.totalPrice)}</td>
-                                            <td className={styles.td_remove}>
-                                                <DeleteIcon className={styles.icon} onClick={() => deleteCartFunc(e?.id)} />
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </Grid>
+                    <>
+                        <Grid container className={styles.cart_container}>
 
-                        <Grid item xs={IsMobile ? 12 : 4} className={styles.cart_total_grid}>
-                            <Box className={styles.total_box_main}>
-                                <Typography className={styles.heading}>CART TOTAL</Typography>
-                                <Box className={styles.total_box}>
-                                    <Typography className={styles.total}>SUB TOTAL:</Typography>
-                                    <Typography className={styles.total_num}>{priceWithCommas(`${cartTotal}`)}</Typography>
+                            <Grid item xs={IsMobile ? 12 : 8} className={styles.cart_grid}>
+                                <table className={styles.table}>
+                                    <thead className={styles.thead}>
+                                        <tr className={styles.trow}>
+                                            <th className={styles.th_image}>{IsMobile ? "" : "Image"}</th>
+                                            <th className={styles.th_name}>{IsMobile ? "Name" : "Product Name"}</th>
+                                            <th className={styles.th_quantity}>Quantity</th>
+                                            <th className={styles.th_price}>Price</th>
+                                            <th className={styles.th_remove}>{IsMobile ? "" : "Remove"}</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className={styles.tbody}>
+                                        {groupedCart.map((e: any, i: number) => (
+                                            <tr className={styles.trow} key={i}>
+                                                <td className={styles.td_image_box}>
+                                                    <img src={optimizeImage(e?.product?.images[0], 'h_150', 'w_150')} alt="" className={styles.image} />
+                                                </td>
+                                                <td className={styles.td_name}>
+                                                    {IsMobile ? add3Dots(e?.product?.product_name, 15) : add3Dots(e?.product?.product_name, 50)}
+                                                </td>
+                                                <td className={styles.td_quantity}>{e?.quantity}</td>
+                                                <td className={styles.td_price}>{priceWithCommas(e?.totalPrice)}</td>
+                                                <td className={styles.td_remove}>
+                                                    <DeleteIcon className={styles.icon} onClick={() => deleteCartFunc(e?.id)} />
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </Grid>
+
+                            <Grid item xs={IsMobile ? 12 : 4} className={styles.cart_total_grid}>
+                                <Box className={styles.total_box_main}>
+                                    <Typography className={styles.heading}>CART TOTAL</Typography>
+                                    <Box className={styles.total_box}>
+                                        <Typography className={styles.total}>SUB TOTAL:</Typography>
+                                        <Typography className={styles.total_num}>{priceWithCommas(`${cartTotal}`)}</Typography>
+                                    </Box>
+                                    <Box className={styles.total_box}>
+                                        <Typography className={styles.total}>TOTAL:</Typography>
+                                        <Typography className={styles.total_num}>{priceWithCommas(`${cartTotal}`)}</Typography>
+                                    </Box>
+                                    <Button className={styles.checkout_button} onClick={handleOpen}>PROCEED TO CHECKOUT</Button>
                                 </Box>
-                                <Box className={styles.total_box}>
-                                    <Typography className={styles.total}>TOTAL:</Typography>
-                                    <Typography className={styles.total_num}>{priceWithCommas(`${cartTotal}`)}</Typography>
-                                </Box>
-                                <Button className={styles.checkout_button}>PROCEED TO CHECKOUT</Button>
-                            </Box>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                        <Modal
+                            className={styles.popup_main}
+                            open={open}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box className={styles.popup_box}>
+                                <Box className={styles.popup_header}>
+                                    <Typography className={styles.heading}>Order Form</Typography>
+                                    <Typography className={styles.icon_box}><CloseIcon className={styles.icon}  onClick={handleClose} /></Typography>
+                                </Box>
+                                <form action="" className={styles.form_box} onSubmit={handlesubmit}>
+                                    <Box className={styles.input_main}>
+                                        <label htmlFor="Name" className={styles.input_label}>Name:</label>
+                                        <input type="text" id="Name" placeholder="asad"  className={styles.inputs} onChange={(e:any)=>setName(e?.taret?.value)} required />
+                                    </Box>
+                                    <Box className={styles.input_main}>
+                                        <label htmlFor="Email" className={styles.input_label}>Email:</label>
+                                        <input type="email" id="Email" placeholder="asad@yopmail.com"  className={styles.inputs}  onChange={(e:any)=>setEmail(e?.taret?.value)} required />
+                                    </Box>
+                                    <Box className={styles.input_main}>
+                                        <label htmlFor="Phone" className={styles.input_label}>Phone No:</label>
+                                        <input type="number" id="Phone" placeholder="Phone No"  className={styles.inputs}  onChange={(e:any)=>setPhone(e?.taret?.value)} required />
+                                    </Box>
+                                    <Box className={styles.input_main}>
+                                        <label htmlFor="Order" className={styles.input_label}>Order Note:</label>
+                                        <input type="text" id="Order" placeholder="Order Note"  className={styles.inputs}  onChange={(e:any)=>setOrderNote(e?.taret?.value)} required />
+                                    </Box>
+                                    <Box className={styles.input_main}>
+                                        <label htmlFor="Address" className={styles.input_label}>Address:</label>
+                                        <textarea id="Address" placeholder="Address" className={styles.textarea}  onChange={(e:any)=>setAddress(e?.taret?.value)} required ></textarea>
+                                    </Box>
+                                    <Box className={styles.input_main}>
+                                        <select name="" id="" className={styles.select_main} onChange={(e:any)=>setPaymentMethod(e?.target?.value)}>
+                                            <option value="" className={styles.select_options} selected disabled hidden>Select Payment Method</option>
+                                            <option value="cash" className={styles.select_options}>Cash on Delivery</option>
+                                            <option value="easy paisa" className={styles.select_options}>Easy Paisa Transfer</option>
+                                            <option value="bank account" className={styles.select_options}>Bank Account Transfer</option>
+                                        </select>
+                                    </Box>
+                                    <Button disableRipple className={styles.submit_btn} type="submit" >Submit</Button>
+                                </form>
+                            </Box>
+                        </Modal>
+                    </>
                     :
                     <div className={styles.load_main}>
                         <div className={styles.load_div}>
--- src/ebikeShop/ShopSharedComponent/filters/index.tsx
+++ src/ebikeShop/ShopSharedComponent/filters/index.tsx
@@ -1,37 +1,31 @@
 "use client";
+
 import { getProductByFilter, getProductCompany } from "@/ebikeShop/Shopfunctions/globalFuntions";
 import Loader from "@/ebikeWeb/sharedComponents/loader/loader";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Modal, Typography } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 import { useState, useEffect } from "react";
 import styles from "./index.module.scss";
-import MoreOptionPopup from "./Popup";
 
-// function Filters({ mainCategoryData, selectedCategoryId }: any) {
 function Filters({ props, mainCategoryData }: any) {
-
   const [productCompanyData, setProductCompanyData] = useState<any>([]);
   const [filter, setFilter] = useState<any>({ company_filter: [] });
   const [companyData, setCompanyData] = useState<any>([]);
   const [modalOpenFor, setModalOpenFor] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
   const [popupData, setPopupData] = useState<any>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const searchParams = useSearchParams();
-
   const router = useRouter();
-
 
   const [selectedCategory, setSelectedCategory] = useState<string>(
     props?.selectedCategoryId || null
   );
 
-
   const [selectedCompany, setSelectedCompany] = useState<string | null>(
     searchParams.get("company")
   );
-
 
   useEffect(() => {
     fetchProductCompanies()
@@ -59,6 +53,7 @@ function Filters({ props, mainCategoryData }: any) {
   const toggle = (from: string) => {
     if (from === "category") {
       setModalOpenFor(from);
+      setPopupData(mainCategoryData);
       setOpenModal(true);
     } else if (from === "company") {
       setModalOpenFor(from);
@@ -70,12 +65,6 @@ function Filters({ props, mainCategoryData }: any) {
     }
   };
 
-  const ModalData = {
-    showmodal: toggle,
-    openmodal: openModal,
-    popupdata: popupData,
-  };
-
   const handleCategoryCheck = ({ data }: any) => {
     let urlTitle = "" + data?.name.toLowerCase().replaceAll(" ", "-");
     router.push(`/shop/collection/${urlTitle}/${data?.id}`);
@@ -85,16 +74,13 @@ function Filters({ props, mainCategoryData }: any) {
     if (event.target.checked) {
       setFilter((prevFilter: any) => {
         const updatedCompanyFilter = [...(prevFilter.company_filter || []), id.toString()];
-
         const obj = {
-          data:
-          {
+          data: {
             amount_filter: "",
             company_filter: updatedCompanyFilter,
             onsell_filter: false
           }
-        }
-        // console.log("data" , obj)
+        };
         sendFilterToAPI(obj);
         return {
           ...prevFilter,
@@ -104,16 +90,13 @@ function Filters({ props, mainCategoryData }: any) {
     } else {
       setFilter((prevFilter: any) => {
         const updatedCompanyFilter = prevFilter.company_filter.filter((item: any) => item !== id.toString());
-
         const obj = {
-          data:
-          {
+          data: {
             amount_filter: "",
             company_filter: updatedCompanyFilter,
             onsell_filter: false
           }
-        }
-        // console.log("data" , obj)
+        };
         sendFilterToAPI(obj);
         return {
           ...prevFilter,
@@ -124,22 +107,21 @@ function Filters({ props, mainCategoryData }: any) {
   };
 
   const sendFilterToAPI = async (filterObj: any) => {
-
     try {
       const filterProduct = await getProductByFilter(filterObj);
       console.log("data:", filterProduct);
-
       if (filterProduct && filterProduct.length > 0) {
         props.setFilteredData(filterProduct)
-      }
-
-      else {
+      } else {
         props.setFilteredData()
       }
-    }
-    catch (error) {
+    } catch (error) {
       console.error("API Error:", error);
     }
+  }
+
+  const submitfilterdata = () => {
+    toggle('close');
   }
 
   return (
@@ -151,7 +133,6 @@ function Filters({ props, mainCategoryData }: any) {
           </Box>
 
           {/* Category Filter */}
-
           <Box className={styles.heading_city}>
             <Typography className={styles.city_text}>CATEGORY</Typography>
           </Box>
@@ -160,7 +141,6 @@ function Filters({ props, mainCategoryData }: any) {
             {
               mainCategoryData?.map((data: any, i: any) => (
                 <Typography className={styles.option_values} key={i}>
-
                   <input
                     type="radio"
                     checked={selectedCategory === data?.id.toString()}
@@ -170,13 +150,10 @@ function Filters({ props, mainCategoryData }: any) {
                   />
                   {data?.name}
                 </Typography>
-
               ))}
-
           </Box>
 
           {/* Company Filter */}
-
           <Box className={styles.heading_brand}>
             <Typography className={styles.brand_text}> Product Company </Typography>
           </Box>
@@ -184,36 +161,68 @@ function Filters({ props, mainCategoryData }: any) {
           <Box className={styles.brand_options}>
             {productCompanyData.slice(0, 5).map((data: any, i: any) => (
               <Typography className={styles.option_values} key={i}>
-
                 <input
                   type="checkbox"
                   id={data.id.toString()}
                   name="company"
                   onChange={(e) => getCompanyDataByFilter(e, data?.id)}
                 />
-
                 {data?.name}
               </Typography>
-
             ))}
-
             <p onClick={() => toggle("company")} className={styles.seeMore}>
               More Company
             </p>
-
           </Box>
 
-          {/* Popup Modal */}
-          {openModal && (
-            <MoreOptionPopup
-              getCompanyDataByFilter={(e: any, id: any) => getCompanyDataByFilter(e, id)}
-              modalData={ModalData}
-              from={modalOpenFor === "category" ? "category" : "company"}
-              filterdData={modalOpenFor === "category" ? selectedCategory : selectedCompany}
-            />
-          )}
-        </>
+          {/* Custom Modal for More Options */}
+          <Modal open={openModal} onClose={() => toggle("close")}>
+            <Box className={styles.Modal_box}>
+              <Box className={styles.modal_header}>
+                <Typography className={styles.slesctMake_heading}>
+                  {modalOpenFor === 'category' ? 'Select Category' : 'Select Company'}
+                </Typography>
+                <Typography className={styles.close_ICon}>
+                  <CloseIcon onClick={() => toggle("close")} />
+                </Typography>
+              </Box>
 
+              <Box className={styles.modal_content}>
+                {
+                  popupData.map((data: any, i: any) => {
+                    return (
+                      <Typography className={styles.option_values} key={i}>
+                        <input
+                          type={modalOpenFor === "category" ? "radio" : "checkbox"}
+                          name={modalOpenFor}
+                          value={data?.id}
+                          id={data?.id.toString()}
+                          checked={
+                            modalOpenFor === "category"
+                              ? selectedCategory === data?.id.toString()
+                              : filter.company_filter.includes(data?.id.toString())
+                          }
+                          onChange={
+                            modalOpenFor === "category"
+                              ? () => handleCategoryCheck({ data })
+                              : (e) => getCompanyDataByFilter(e, data?.id)
+                          }
+                        />
+                        {data?.name}
+                      </Typography>
+                    )
+                  })
+                }
+              </Box>
+
+              <Box className={styles.modal_footer}>
+                <Button className={styles.btn_submit} onClick={submitfilterdata}>
+                  Submit
+                </Button>
+              </Box>
+            </Box>
+          </Modal>
+        </>
       ) : (
         <div className={styles.load_main}>
           <div className={styles.load_div}>
