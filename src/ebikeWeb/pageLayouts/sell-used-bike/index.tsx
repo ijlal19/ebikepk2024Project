397977477aef906becdd97652bb587c92a92b403
abdullah30nov <abdullahbinaslam64@gmail.com>
2025-09-09 23:53:56 +0500

    Update

--- src/ebikeWeb/pageLayouts/sell-used-bike/index.tsx
+++ src/ebikeWeb/pageLayouts/sell-used-bike/index.tsx
@@ -65,7 +65,7 @@ const SellUsedBike = () => {
     };
 
     const handelsubmit = async () => {
-
+  
         if (!customer || customer == "not_login" || customer?.id == undefined) {
             Router.push('/')
         }
