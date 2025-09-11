beb40bd0f59817dcb6777d6c9940db1b0a39daca
abdullah30nov <abdullahbinaslam64@gmail.com>
2025-09-12 01:34:16 +0500

    update

--- src/ebikeWeb/sharedComponents/new_item_card/index.tsx
+++ src/ebikeWeb/sharedComponents/new_item_card/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './index.module.scss';
 import { Link, useMediaQuery } from '@mui/material';
-import { add3Dots } from '@/genericFunctions/geneFunc';
+import { add3Dots, priceWithCommas } from '@/genericFunctions/geneFunc';
 
 const NewCard = ({ props }: any) => {
     const isMobile = useMediaQuery('(max-width:768px)')
@@ -11,27 +11,79 @@ const NewCard = ({ props }: any) => {
     };
     return (
         <div className={styles.main}>
-             <div className={styles.container}>
-            <div className={styles.image_box} 
-            // style={{
-            //     background: `url(${props?.featuredImage?.split(' #$# ')[0]?.trim()})`,
-            //     backgroundPosition: 'center',
-            //     backgroundRepeat: 'no-repeat',
-            //     backgroundSize: '100% auto'
-            // }} 
-            >
-                <img src={props?.featuredImage?.split(' #$# ')[0]?.trim()} alt={props?.blogTitle} className={styles.image} />
+            <div className={styles.container}>
+                <div className={styles.image_box}
+                >
+                    <img src={props?.featuredImage?.split(' #$# ')[0]?.trim()} alt={props?.blogTitle} className={styles.image} />
+                </div>
+                <div className={styles.content}>
+                    <p className={styles.title}>{add3Dots(props?.blogTitle, isMobile ? 25 : 60)}</p>
+                    <p className={styles.author}>By <span className={styles.name}>{props?.authorname}</span></p>
+                    <p className={styles.description}>{add3Dots(props?.meta_description, isMobile ? 40 : 80)}</p>
+                    <Link href={getBlogUrl(props)} className={styles.link}>
+                        View Detail
+                    </Link>
+                </div>
             </div>
-            <div className={styles.content}>
-                <p className={styles.title}>{add3Dots(props?.blogTitle , isMobile? 25 : 60)}</p>
-                <p className={styles.author}>By <span className={styles.name}>{props?.authorname}</span></p>
-                <p className={styles.description}>{add3Dots(props?.meta_description, isMobile?40 : 80)}</p>
-                <Link href={getBlogUrl(props)} className={styles.link}>
-                    {/* <button className={styles.btn}> */}
-                    View Detail
-                    {/* </button> */}
-                </Link>
+        </div>
+    )
+}
+
+const NewBikeCard = ({ props }: any) => {
+    const isMobile = useMediaQuery('(max-width:768px)')
+   
+    return (
+        <div className={styles.main}>
+            <div className={styles.container}>
+                <div className={styles.image_boxnewbike}>
+                    <img src={props?.img_url} alt={props?.title} className={styles.image} />
+                </div>
+                <div className={styles.content}>
+                    <p className={styles.title}>{add3Dots(props?.title, isMobile ? 15 : 28)}</p>
+                    <p className={styles.price}><span className={styles.name}>{props?.price}</span></p>
+                    {/* <Link href={getBlogUrl(props)} className={styles.link}> */}
+                    <Link href={props?.url} className={styles.linkbtn}>
+                        View Detail
+                    </Link>
+                </div>
             </div>
+        </div>
+    )
+}
+
+const UsedBikeCard = ({ props }: any) => {
+    const isMobile = useMediaQuery('(max-width:768px)')
+    const GetImageSrc = (image:any) => {
+        if(image && image.length > 0){
+            return image[0]
+        }
+        else{
+            return "https://res.cloudinary.com/dtroqldun/image/upload/c_scale,f_auto,h_200,q_auto,w_auto,dpr_auto/v1549082792/ebike-graphics/placeholders/used_bike_default_pic.png"
+        }
+    }
+    const GetHref = (val:any) => {
+            let title = val.title
+            let urlTitle = '' + title.toLowerCase().replaceAll(' ', '-')
+            return `/used-bikes/${urlTitle}/${val.id}`
+        }
+    return (
+        <div className={styles.main}>
+            <div className={styles.container}>
+                <div className={styles.image_boxnewbike}>
+                    <img
+                        src={GetImageSrc(props?.images)}
+                        alt={props?.title || "default bike"}
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <p className={styles.title}>{add3Dots(props?.title, isMobile ? 15 : 28)}</p>
+                    <p className={styles.price}><span className={styles.name}>PKR {priceWithCommas(props?.price)}</span></p>
+                    {/* <Link href={getBlogUrl(props)} className={styles.link}> */}
+                    <Link href={GetHref(props)} className={styles.linkbtn}>
+                        View Detail
+                    </Link>
+                </div>
             </div>
         </div>
     )
@@ -42,27 +94,20 @@ const NewVideoCard = ({ props }: any) => {
     return (
         <div className={styles.main}>
             <div className={styles.container}>
-            <div className={styles.image_box} 
-            // style={{
-            //     background: `url(${props?.featuredImage?.split(' #$# ')[0]?.trim()})`,
-            //     backgroundPosition: 'center',
-            //     backgroundRepeat: 'no-repeat',
-            //     backgroundSize: '100% auto'
-            // }} 
-            >
-                <img src={props?.thumbnail_url} alt={props?.title} className={styles.image} />
-            </div>
-            <div className={styles.content}>
-                <p className={styles.title}>{add3Dots(props?.title , isMobile? 25 : 60)}</p>
-                <p className={styles.author}>By <span className={styles.name}>Ebike Team</span></p>
-                {/* <p className={styles.description}>{add3Dots(props?.meta_description, isMobile?40 : 80)}</p> */}
-                <Link href={props?.video_url} target="_blank" className={styles.link1}>
-                    Watch Video
-                </Link>
-            </div>
+                <div className={styles.image_box}
+                >
+                    <img src={props?.thumbnail_url} alt={props?.title} className={styles.image} />
+                </div>
+                <div className={styles.content}>
+                    <p className={styles.title}>{add3Dots(props?.title, isMobile ? 25 : 60)}</p>
+                    <p className={styles.author}>By <span className={styles.name}>Ebike Team</span></p>
+                    <Link href={props?.video_url} target="_blank" className={styles.link1}>
+                        Watch Video
+                    </Link>
+                </div>
             </div>
         </div>
     )
 }
 
-export { NewCard  ,NewVideoCard}
+export { NewCard, NewBikeCard, NewVideoCard, UsedBikeCard }
