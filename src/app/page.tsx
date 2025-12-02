784bf002d0a9722be7ed0f8f75e3c0255d477fb8
∆𝔟𝔡𝔲𝔩𝔩@𝔥 𝔅î𝔫 ∆𝔰𝔩𝔞𝔪 <abdulahad30396@gmail.com>
2025-12-02 09:59:39 +0500

    work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,22 @@ import Index from '@/ebikeWeb/pageLayouts/home'
 import * as React from 'react';
 import { Metadata } from 'next'
 
-export async function generateMetadata(): Promise<Metadata> {
+export const metadata: Metadata = {
+  title: "ebike.pk – Buy & Sell New & Used Bikes in Pakistan | Latest Bike Prices 2025",
+  description: "Get Complete Details about Bikes / Motorcycles, New Bikes, Bike Parts, Used Bikes, Forum, & Motorcycle News / Videos in Pakistan. Post Ad for FREE",
 
-    return {
-      // title: "Pakistan's Ist Exclusive Motorcycle Portal",
-      title: "ebike.pk – Buy & Sell New & Used Bikes in Pakistan | Latest Bike Prices 2025",
-      description: "Get Complete Details about Bikes / Motorcycles, New Bikes, Bike Parts, Used Bikes, Forum, & Motorcycle News / Videos in Pakistan. Post Ad for FREE",
-      // description: "Get Complete Details about Bikes / Motorcycles, New Bikes, Bike Parts, Used Bikes, Forum, & Motorcycle News / Videos in Pakistan. Post Ad for FREE",
-      openGraph: {
-        title: "ebike.pk – Buy & Sell New & Used Bikes in Pakistan | Latest Bike Prices 2025",
-        description: "Get Complete Details about Bikes / Motorcycles, New Bikes, Bike Parts, Used Bikes, Forum, & Motorcycle News / Videos in Pakistan. Post Ad for FREE",
-      },
-    }
+  openGraph: {
+    title: "ebike.pk – Buy & Sell New & Used Bikes in Pakistan | Latest Bike Prices 2025",
+    description: "Get Complete Details about Bikes / Motorcycles, New Bikes, Bike Parts, Used Bikes, Forum, & Motorcycle News / Videos in Pakistan. Post Ad for FREE",
+    url: "https://www.ebike.pk/",
+    siteName: "ebike.pk",
+    type: "website",
+  },
+
+  alternates: {
+    canonical: "https://www.ebike.pk/",
   }
+};
 
 export default function Home() {
   return (
