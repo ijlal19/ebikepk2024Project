893775fc30e317aec62452bdeaa4eea86642717a
Abdulla30nov <abdullahbinaslam64@gmail.com>
2025-12-14 05:17:33 +0500

    update

--- src/app/used-bikes/page.tsx
+++ src/app/used-bikes/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next'
 import { getBrandFromId, getCityFromId, getCustomBikeAd } from "@/ebikeWeb/functions/globalFuntions";
 
 export async function generateMetadata(): Promise<Metadata> {
-
+ 
   return {
     title: 'Used Bikes for Sale in Pakistan | ebike.pk',
     description: 'Used bikes for sale in Pakistan. Find old model bikes and second hand motorcycles for sale in Pakistan. The best used bikes for sale in Karachi and Lahore.',
