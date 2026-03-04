9dc17809a3f6c51b8c33984642841311ff232330
Abdul Ahad <abdulahad7ctech@gmail.com>
2026-03-04 22:54:25 +0500

    Update page.tsx

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import Home from '@/ebikeForum/forumPages/home';
 import Header from '@/ebikeForum/forumSharedComponent/header';
-import Head from 'next/head';
-export default function forum() {
-    <Head>
-        <meta name="robots" content="noindex, nofollow" />
-        <title> Forum </title>
-    </Head>
+
+export default function ForumPage() {
     return (
         <>
             <Header />
-            <Home />
+            <React.Suspense fallback={null}>
+                <Home />
+            </React.Suspense>
         </>
-    )
+    );
 }
